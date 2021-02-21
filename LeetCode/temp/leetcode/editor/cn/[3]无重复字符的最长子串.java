d3273416cb8c8088b38014cd7170bd55ae736de8
hl <PrivateHL@163.com>
2021-02-21 19:51:22 +0800

    LeetCode

---
+++ LeetCode/temp/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,64 @@
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: s = ""
+//输出: 0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 104 
+// s 由英文字母、数字、符号和空格组成 
+// 
+// Related Topics 哈希表 双指针 字符串 Sliding Window 
+// 👍 4983 👎 0
+
+
+import java.util.HashSet;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //由左右两边的游标left/right,同时从头开始 先游动right直到找到重复字符，得到一个长度，left移到被重复的字符位置，再循环移动right，得到一系列长度，取到最大值即可。
+        //使用set来判断当前left-right位置，含有哪些字符，可以快速的判断是否重复。
+        int left = 0, right = 0;
+        HashSet<char> charSet = new HashSet<char>();
+        for (int i = 0; i <s.length ; i++) {
+
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
