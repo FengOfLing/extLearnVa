4a157132fd8272725b2e0d979e738fbc933fb3f3
PrivateHL <PrivateHL@163.com>
2021-02-25 11:28:22 +0800

    leet code 867

---
+++ leetCode/tempHL/leetcode/editor/cn/[867]转置矩阵.java
@@ -0,0 +1,54 @@
+//给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。 
+//
+// 矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//输出：[[1,4,7],[2,5,8],[3,6,9]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [
+// [1,2,3],
+// [4,5,6] int[3][2]  m.length=2 m[0].length=3
+// ]
+//输出：[[1,4],[2,5],[3,6]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 1000 
+// 1 <= m * n <= 105 
+// -109 <= matrix[i][j] <= 109 
+// 
+// Related Topics 数组 
+// 👍 162 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        //简单的办法 直接新建个二维数组，一个一个遍历放置数据
+        int[][] result = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
