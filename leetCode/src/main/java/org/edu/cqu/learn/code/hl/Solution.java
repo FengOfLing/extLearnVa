d57f7d44947718b203919e4c4aa6197a3b76834d
PrivateHL <PrivateHL@163.com>
2021-03-02 17:01:46 +0800

    leet code 14

--- leetCode/src/main/java/org/edu/cqu/learn/code/hl/Solution.java
+++ leetCode/src/main/java/org/edu/cqu/learn/code/hl/Solution.java
@@ -4,35 +4,21 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println( romanToInt("LVIII")); //58
-        System.out.println( romanToInt("IX")); //9
-        System.out.println( romanToInt("MCMXCIV")); //1994
+        System.out.println( longestCommonPrefix(new String[]{"dog","racecar","car"})); //58
+        System.out.println( longestCommonPrefix(new String[]{"flower","flow","flight"})); //58
     }
-    public static int romanToInt(String s) {
-        int result = 0;
-        int currentVal = -1;
-
-        //由右向左遍历，如果左边的始终比当前的val表示的值小，那就减 ，大就加
-        for (int i = s.length()-1; i >= 0; i--) {
-            int val = getVal(s.charAt(i));
-            if(val >= currentVal){
-                result += val;
-                currentVal = val;
-            }else{
-                result -= val;
+    public static String longestCommonPrefix(String[] strs) {
+        int i = 0;
+        found:
+        for ( ; i < strs[0].length() ; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if( i >= strs[j].length() || strs[j].charAt(i) != c ){
+                    break found;
+                }
             }
         }
-        return result;
-    }
-    public static int getVal( char c){
-        if( c == 'I') return 1;
-        if( c == 'V') return 5;
-        if( c == 'X') return 10;
-        if( c == 'L') return 50;
-        if( c == 'C') return 100;
-        if( c == 'D') return 500;
-        if( c == 'M') return 1000;
-        return 0;
+        return i == 0 ? "" : strs[0].substring(0,i);
     }
 }
 
