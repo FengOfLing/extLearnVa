0486c179053086053033407455b14768e2f15229
PrivateHL <PrivateHL@163.com>
2021-03-02 13:08:42 +0800

    leet code 13

--- leetCode/src/main/java/org/edu/cqu/learn/code/hl/Solution.java
+++ leetCode/src/main/java/org/edu/cqu/learn/code/hl/Solution.java
@@ -4,21 +4,35 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(reverse(-123)); //-321
-        System.out.println(reverse(1534236469)); //0
-        System.out.println(reverse(1463847412)); //2147483641
+        System.out.println( romanToInt("LVIII")); //58
+        System.out.println( romanToInt("IX")); //9
+        System.out.println( romanToInt("MCMXCIV")); //1994
     }
-    public static int reverse(int x) {
+    public static int romanToInt(String s) {
         int result = 0;
-        while (x != 0){
-            int a = x % 10;
-            if( x > 0 && (Integer.MAX_VALUE - a ) / 10 < result) return 0;
-            if( x < 0 && (Integer.MIN_VALUE - a ) / 10 > result) return 0;
-            result = result * 10 + a;
-            x = x/10;
+        int currentVal = -1;
+
+        //由右向左遍历，如果左边的始终比当前的val表示的值小，那就减 ，大就加
+        for (int i = s.length()-1; i >= 0; i--) {
+            int val = getVal(s.charAt(i));
+            if(val >= currentVal){
+                result += val;
+                currentVal = val;
+            }else{
+                result -= val;
+            }
         }
         return result;
     }
-
+    public static int getVal( char c){
+        if( c == 'I') return 1;
+        if( c == 'V') return 5;
+        if( c == 'X') return 10;
+        if( c == 'L') return 50;
+        if( c == 'C') return 100;
+        if( c == 'D') return 500;
+        if( c == 'M') return 1000;
+        return 0;
+    }
 }
 
