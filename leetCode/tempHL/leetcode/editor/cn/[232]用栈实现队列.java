688ba072400a55d727e2e689aacc268675fc4f44
PrivateHL <PrivateHL@163.com>
2021-03-05 17:41:45 +0800

    leet code 232

--- leetCode/tempHL/leetcode/editor/cn/[232]用栈实现队列.java
+++ leetCode/tempHL/leetcode/editor/cn/[232]用栈实现队列.java
@@ -68,7 +68,6 @@ class MyQueue {
 
     Stack<Integer> seq ;
     Stack<Integer> rev ;
-    int deletenum = 0;//已经从rev移除的个数
     /** Initialize your data structure here. */
     public MyQueue() {
         seq = new Stack<>();
@@ -82,16 +81,23 @@ class MyQueue {
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if( !rev.isEmpty()){
-//            deletenum++;
-            return rev.pop();
-        }else{
-            trans();
-            if( rev.isEmpty()){
-                return 0;
-            }
-            return rev.pop();
+        trans();
+        if( rev.isEmpty()){
+            return 0;
         }
+        return rev.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        trans();
+        return rev.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        trans();
+        return rev.isEmpty();
     }
 
     private void trans(){
@@ -100,30 +106,6 @@ class MyQueue {
             while(!seq.isEmpty()){
                 rev.push( seq.pop());
             }
-//            while(deletenum > 0){
-//                rev.pop();
-//                deletenum--;
-//            }
-        }
-    }
-
-    /** Get the front element. */
-    public int peek() {
-        if( !rev.isEmpty()){
-            return rev.peek();
-        }else{
-            trans();
-            return rev.peek();
-        }
-    }
-
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        if( rev.isEmpty()){
-            trans();
-            return rev.isEmpty();
-        }else{
-            return false;
         }
     }
 }
