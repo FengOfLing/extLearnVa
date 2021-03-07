b6fcb175e62e8e1f34c7a3e092a271a0f93fb375
hl <PrivateHL@163.com>
2021-03-07 23:34:29 +0800

    LeetCode 131

--- leetCode/tempHL/leetcode/editor/cn/[131]分割回文串.java
+++ leetCode/tempHL/leetcode/editor/cn/[131]分割回文串.java
@@ -1,12 +1,40 @@
-package org.edu.cqu.learn.code.hl;
+//给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。 
+//
+// 回文串 是正着读和反着读都一样的字符串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "aab"
+//输出：[["a","a","b"],["aa","b"]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "a"
+//输出：[["a"]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 16 
+// s 仅由小写英文字母组成 
+// 
+// Related Topics 深度优先搜索 动态规划 回溯算法 
+// 👍 605 👎 0
 
-import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) {
-
-    }
-
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //			解答成功:
+    //			执行耗时:32 ms,击败了5.17% 的Java用户
+    //			内存消耗:66.8 MB,击败了5.00% 的Java用户
     public List<List<String>> partition(String s) {
         return getSubHuiWen(s,0,s.length()-1);
     }
@@ -23,7 +51,7 @@ public class Solution {
         for (int i = start; i <= end; i++) {
             for (int j = i + 1; j <= end ; j++) {
                 if( isHuiWen(s, i, j)){
-                    List<List<String>> subResult = getSubHuiWen(s,j,end);
+                    List<List<String>> subResult = getSubHuiWen(s,j+1,end);
                     for (int k = 0; k < subResult.size(); k++) {
                         List<String> list = new ArrayList<String>();
                         for (int l = start; l < i ; l++) {
@@ -39,6 +67,7 @@ public class Solution {
         return result;
     }
 
+
     public boolean isHuiWen(String s, int start, int end){
         while( start < end ){
             if( s.charAt(start) != s.charAt(end)){
@@ -48,7 +77,7 @@ public class Solution {
             end--;
 
         }
+        return true;
     }
-
 }
-
+//leetcode submit region end(Prohibit modification and deletion)
