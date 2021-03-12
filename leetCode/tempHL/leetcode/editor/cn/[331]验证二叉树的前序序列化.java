059fbef162d74ff609013c91fc1bbe7efd7b830f
PrivateHL <PrivateHL@163.com>
2021-03-12 11:49:46 +0800

    leet code 331

--- leetCode/tempHL/leetcode/editor/cn/[331]验证二叉树的前序序列化.java
+++ leetCode/tempHL/leetcode/editor/cn/[331]验证二叉树的前序序列化.java
@@ -1,14 +1,47 @@
-package org.edu.cqu.learn.code.hl;
+//序列化二叉树的一种方法是使用前序遍历。当我们遇到一个非空节点时，我们可以记录下这个节点的值。如果它是一个空节点，我们可以使用一个标记值记录，例如 #。 
+//
+//      _9_
+//    /   \
+//   3     2
+//  / \   / \
+// 4   1  #  6
+/// \ / \   / \
+//# # # #   # #
+// 
+//
+// 例如，上面的二叉树可以被序列化为字符串 "9,3,4,#,#,1,#,#,2,#,6,#,#"，其中 # 代表一个空节点。 
+//
+// 给定一串以逗号分隔的序列，验证它是否是正确的二叉树的前序序列化。编写一个在不重构树的条件下的可行算法。 
+//
+// 每个以逗号分隔的字符或为一个整数或为一个表示 null 指针的 '#' 。 
+//
+// 你可以认为输入格式总是有效的，例如它永远不会包含两个连续的逗号，比如 "1,,3" 。 
+//
+// 示例 1: 
+//
+// 输入: "9,3,4,#,#,1,#,#,2,#,6,#,#"
+//输出: true 
+//
+// 示例 2: 
+//
+// 输入: "1,#"
+//输出: false
+// 
+//
+// 示例 3: 
+//
+// 输入: "9,#,#,1"
+//输出: false 
+// Related Topics 栈 
+// 👍 195 👎 0
 
-import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println( isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#") );
-        System.out.println( isValidSerialization("1,#") );
-        System.out.println( isValidSerialization("9,#,#,1") );
-    }
-    public static boolean isValidSerialization(String preorder) {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //			解答成功:
+    //			执行耗时:5 ms,击败了38.91% 的Java用户
+    //			内存消耗:37 MB,击败了81.21% 的Java用户
+    public boolean isValidSerialization(String preorder) {
         //前序遍历，中左右  遇到null 下一个元素是上一个非空节点的右子节点。
         Character nullChar = '#';
         Stack<Integer> nodeIndex = new Stack();
@@ -50,6 +83,5 @@ public class Solution {
         }
         return nodeIndex.isEmpty();
     }
-
 }
-
+//leetcode submit region end(Prohibit modification and deletion)
